7546d896e8e205ef81b271ae7e5dac2db14b3e1d
Stefan Lesser <mail@stefan-lesser.com>
2017-06-08 14:30:27 -0400

    Experimenting with simple terminal drawing.

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,60 @@
 module Main where
 
+import Tron
+import System.Console.ANSI
+import System.IO
+
+drawPixel :: Int -> Int -> IO ()
+drawPixel x y = do
+  setCursorPosition y x
+  setSGR [SetColor Background Vivid White]
+  putStr " "
+
+getInput :: IO (Char)
+getInput = do
+  char <- getChar
+  return char
+
+nextState :: Position -> IO ()
+nextState pos@(Position x y) = do
+  gameLoop pos
+
+-- game loop
+gameLoop :: Position -> IO ()
+gameLoop pos@(Position x y) = do
+  -- some test output
+  drawPixel x y
+
+  -- process input character
+  input <- getInput
+  case input of
+    'q' -> handleExit
+    'w' -> nextState (Position x (y - 1))
+    'a' -> nextState (Position (x - 1) y)
+    's' -> nextState (Position x (y + 1))
+    'd' -> nextState (Position (x + 1) y)
+    _   -> nextState pos
+
+-- handle exit
+handleExit :: IO ()
+handleExit = do
+  setSGR [Reset]
+  clearScreen
+  setCursorPosition 0 0
+  showCursor
+  putStrLn "Thank you for playing!"
+
+-- main function
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  -- set up terminal
+  hSetEcho      stdin  False
+  hSetBuffering stdin  NoBuffering
+  hSetBuffering stdout NoBuffering
+  hideCursor
+  setTitle "hTron"
+
+  -- clear screen and launch into game loop
+  clearScreen
+  gameLoop (Position 10 10)
+
